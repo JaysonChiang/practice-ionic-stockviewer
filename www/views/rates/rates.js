c1a8df975ddbc568b7eca84ee14b9e4132bc6194
Jayson <jayson@mac.local>
2018-02-22 13:13:26 +0800

    refresh and popup

--- www/views/rates/rates.js
+++ www/views/rates/rates.js
@@ -1,23 +1,41 @@
 angular.module('App')
-    .controller('RatesController', function ($scope, $http, Currencies,apiService) {
+    .controller('RatesController', function ($scope, $http, $ionicPopover, Currencies, apiService) {
 
         $scope.currencies = Currencies;
+
+        $ionicPopover.fromTemplateUrl('views/rates/help-popover.html', {
+            scope: $scope,
+        }).then(function (popover) {
+            $scope.popover = popover;
+        })
+
+        $scope.openHelp = function ($event) {
+            $scope.popover.show($event);
+        }
+
+        $scope.$on('$destory', function () {
+            $scope.popover.remove();
+        })
+
 
         $scope.load = function () {
             apiService.getSignature();
             apiService.getData()
-                 .then(function (res) {
-                     var tickers = res.data;
-                     $scope.currencies.forEach(function (currency) {
-                         currency.ticker = tickers[currency.code];
-                         currency.ticker.timestamp = new Date(currency.ticker.timestamp);
-                     })
-                     console.log($scope.currencies);
-                 },function(err){
-                     console.log(err);
-                 })
-         };
-         $scope.load();
+                .then(function (res) {
+                    var tickers = res.data;
+                    $scope.currencies.forEach(function (currency) {
+                        currency.ticker = tickers[currency.code];
+                        currency.ticker.timestamp = new Date(currency.ticker.timestamp);
+                    })
+                    console.log($scope.currencies);
+                }, function (err) {
+                    console.log(err);
+                })
+                .finally(function () {
+                    $scope.$broadcast('scroll.refreshComplete');
+                })
+        };
+        $scope.load();
     })
 
 
